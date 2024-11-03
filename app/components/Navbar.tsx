6cd9a96847e5a590ab41d00a029eae2fc128a07d
Ajirthan Balasingham <ajirbala28@gmail.com>
2024-11-03 20:08:47 +0530

    feat: complete theming & fonts customization

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signIn, signOut } from "@/auth";
 
 const Navbar = async () => {
   const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
